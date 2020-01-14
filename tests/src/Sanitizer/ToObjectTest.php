dad4f80ee10fa40d16c9143f982f772348d31f0c
Arrow <michal.strzelczyk@miinto.com>
2020-01-14 16:54:42 +0100

    add ToObject sanitizer

---
+++ tests/src/Sanitizer/ToObjectTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare (strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class ToObjectTest extends TestCase
+{
+    /**
+     * @dataProvider data
+     */
+    public function testToObject($data, $expected): void
+    {
+        $this->assertEquals($expected , \PayloadValidator\Sanitizer\ToObject::sanitize($data));
+    }
+
+    /**
+     * @return array
+     */
+    public function data()
+    {
+        return [
+            [['test'], (object)['test']],
+            [['name' => 'John Snow'], (object)['name' => 'John Snow']]
+        ];
+    }
+}
